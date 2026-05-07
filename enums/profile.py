123e38aac42b29ec1207ba92c07a69dea7e1fa40
Carlotta <juan.rubio262132@potros.itson.edu.mx>
2026-05-06 19:07:12 -0700

    Add initial project files and structure
    - Implement Profile enum in enums/profile.py
    - Establish database connection in persistence/db.py
    - Set up basic HTML structure in templates/index.html
    - Add Flask application in app.py with a root route

---
+++ enums/profile.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class Profile(Enum):
+    ADMIN = 1
+    PLAYER = 2
--- persistence/db.py
+++ persistence/db.py
@@ -1,1 +1,9 @@
-#Conección a la base de datos
+import pymysql
+
+def get_connection():
+        return pymysql.connect(
+        host='localhost',
+        user='root',
+        password='admin',
+        database='crossover_db'
+    )
